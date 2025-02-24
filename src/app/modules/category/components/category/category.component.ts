4b8d02773351f02d85c64e40ae918d01861c916e
José Luis López G. <lgjoseluis@hotmail.com>
2025-02-24 11:04:53 -0600

    Agregar formulario buscar categorías-maquetar con bootstrap

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoryService } from '../../../shared/services/category.service';
 
 @Component({
   selector: 'app-category',
   standalone: false,
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+  private service: CategoryService = inject(CategoryService);
 
+  ngOnInit(): void {
+    this.getCategories();
+  }  
+
+  getCategories(): void {
+    this.service.getCategories().subscribe({
+      next: (response : any) =>{
+        console.log(response);
+      },
+      error: (error: any)=>{
+        console.log('Error',error);
+      }
+    });
+  }
 }
