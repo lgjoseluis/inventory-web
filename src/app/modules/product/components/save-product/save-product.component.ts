70fb056ffeb40ef3179b9b2e6c3c2e33eee960d9
José Luis López G. <lgjoseluis@hotmail.com>
2025-02-26 16:55:19 -0600

    Agregar lista de categorias al formulario de captura de productos

--- src/app/modules/product/components/save-product/save-product.component.ts
+++ src/app/modules/product/components/save-product/save-product.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Product } from '../product/product.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductService } from '../../../shared/services/product.service';
+import { CategoryService } from '../../../shared/services/category.service';
 
 @Component({
   selector: 'app-save-product',
@@ -15,22 +16,26 @@ export class SaveProductComponent implements OnInit{
   private data = inject<Product>(MAT_DIALOG_DATA);
   private fb = inject(FormBuilder);
   private service = inject(ProductService);
+  private cateService = inject(CategoryService)
 
   public statusForm = "Crear"
   public productForm!: FormGroup;
+  public categories:Category[] = [];
 
   ngOnInit(): void { 
+    this.getCategories();
+
     this.productForm = this.fb.group({
           name:[this.data.name, Validators.required],
           price:[this.data.price, Validators.required],
           account:[this.data.account, Validators.required],
           category:[this.data.category, Validators.required],
-          pcture:[this.data.picture, Validators.required]
+          picture:[this.data.picture, Validators.required]
         });
 
     if(this.data.id===0){
       this.statusForm = 'Actualizar'
-    }
+    }    
   }
 
   onSave(){}
@@ -38,4 +43,23 @@ export class SaveProductComponent implements OnInit{
   onCancel(){
     this.dialogRef.close(2);
   }
+
+  getCategories(){
+    this.cateService.getCategories().subscribe({
+      next: (response : any) =>{
+        this.categories = response.categoryResponse.category;
+      },
+      error: (error: any)=>{
+        console.log('Error', "Error al recuperar las categorías");
+        this.categories = [];
+      }
+    });
+  }
+}
+
+
+export interface Category{
+  id: number;
+  name:string;
+  description:string;  
 }
